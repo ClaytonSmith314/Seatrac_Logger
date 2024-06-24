f2c3d66d823f53a9f09e82959efd5eefd2e5e97d
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-06-24 11:28:35 -0600

    bug fix

--- src/seatrac_logger/seatrac_logger/seatrac_logger.py
+++ src/seatrac_logger/seatrac_logger/seatrac_logger.py
@@ -12,7 +12,7 @@ class SeatracLogger(Node):
     def __init__(self):
         super().__init__('logger')
         self.modem_publisher_  = self.create_publisher(ModemSend, 'modem_send', 10)
-        self.modem_subscriber_ = self.create_subscriber(ModemRec, 'modem_rec', self.modem_callback)
+        self.modem_subscriber_ = self.create_subscription(ModemRec, 'modem_rec', self.modem_callback)
 
         self.i = 0
 
@@ -26,14 +26,9 @@ class SeatracLogger(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-
     seatrac_logger = SeatracLogger()
-
     rclpy.spin(seatrac_logger)
     seatrac_logger.output_file.close()
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     seatrac_logger.destroy_node()
     rclpy.shutdown()
 
