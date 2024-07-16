223e46b8c9c3b2bbedc83aa20bd5884b36ccc7f6
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2024-07-16 13:27:33 -0600

    Derek field test should be working

--- src/Derek_field_test/src/seatrac_pinger.py
+++ src/Derek_field_test/src/seatrac_pinger.py
@@ -10,21 +10,13 @@ CONFIG_FILE_PATH = "./seatrac_logger_config.toml"
 TIMER_PERIOD_SECONDS = 0.01
 
 
-def int_to_bytes_list(num):
-    bytes = [0]*30
-    bytes[0] = (num >> 24) & 0xFF
-    bytes[1] = (num >> 16) & 0xFF
-    bytes[2] = (num >> 8) & 0xFF
-    bytes[3] = num & 0xFF
-    return bytes
-
 class SeatracPinger(Node):
 
     def __init__(self):
         super().__init__('pinger')
 
         with open(CONFIG_FILE_PATH) as config_file:
-            logger_config = toml.load(config_file)["LoggerConfig"]
+            logger_config = toml.load(config_file)["DerekTest"]
 
             self.num_rounds_of_pings = logger_config["num_pings_per_beacon"]
             self.other_beacon_ids    = logger_config["other_beacon_ids"]
@@ -42,10 +34,6 @@ class SeatracPinger(Node):
         self.modem_publisher_  = self.create_publisher(ModemSend, 'modem_send', 10)
         self.modem_subscriber_ = self.create_subscription(ModemRec, 'modem_rec', self.modem_callback, 10)
 
-        self.timer = self.create_timer(TIMER_PERIOD_SECONDS, self.timer_callback)
-        self.time_since_pinged = 0.0
-        self.received_response = False
-
         self.beacon_id_list_index = 0
         self.rounds_of_pings_sent = 0
 
@@ -54,11 +42,9 @@ class SeatracPinger(Node):
 
     def send_ping(self):
         request = ModemSend()
-        request.msg_id      = CID_E.CID_ECHO_SEND
+        request.msg_id      = CID_E.CID_PING_SEND
         request.dest_id     = self.other_beacon_ids[self.beacon_id_list_index]
         request.msg_type    = self.msg_type
-        request.packet_len  = 4
-        request.packet_data = int_to_bytes_list(self.rounds_of_pings_sent)
 
         self.modem_publisher_.publish(request)
 
@@ -70,15 +56,11 @@ class SeatracPinger(Node):
                 pass # TODO: kill the node here
 
     def modem_callback(self, response):
-        self.received_response = True
-    
-    def timer_callback(self):
-        if self.received_response or self.time_since_pinged>self.timeout:
-            self.time_since_pinged = 0.0
-            self.received_response = False
+        if response.msg_id == CID_E.CID_PING_RESP:
             self.send_ping()
-        else:
-            self.time_since_pinged += TIMER_PERIOD_SECONDS
+        if response.msg_id == CID_E.CID_PING_ERROR:
+            self.send_ping()
+            self.get_logger().error("Seatrac Ping Error. CST_E Error Code: %s", str(response.command_status_code))
 
 
 def main(args=None):
